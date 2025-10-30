742c71d6d5e0989cd34b0242c75dddfcd669b396
jahoog <35244949+jahoog@users.noreply.github.com>
2025-10-30 07:41:25 -0400

    Add multi-file

--- pdal_linux/install.sh
+++ pdal_linux/install.sh
@@ -13,6 +13,15 @@ conda create -n prod -y
 conda activate prod
 conda install -c conda-forge pdal python-pdal gdal -y  # Add your packages
 
+#Single File Conversion
 wget -q https://giselevationingov.s3.amazonaws.com/las/statewide/2020/SPW/ql2/IN2020_26550965_12.las
-wget -q https://github.com/jahoog/pdal_test/raw/refs/heads/main/pdal_linux/las_to_copc.py
+wget -q https://raw.githubusercontent.com/jahoog/pdal_test/refs/heads/main/pdal_linux/las_to_copc.py
 python3 las_to_copc.py IN2020_26550965_12.las
+
+# Multi File Conversion from S3 to S3
+wget -q https://raw.githubusercontent.com/jahoog/pdal_test/refs/heads/main/pdal_docker/run.py
+export S3_SOURCE_BUCKET=jah-br-samp-docs
+export S3_SOURCE_FOLDER=las/statewide/2020/SPW/ql2/
+export S3_TARGET_BUCKET=jah-br-samp-docs
+export S3_TARGET_FOLDER=copc/statewide/2020/SPW/ql2/
+python3 run.py
